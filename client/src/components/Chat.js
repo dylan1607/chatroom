27ae49092d397f4087428d1d17a339bad71b787f
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-16 20:58:23 +0700

    update

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -38,54 +38,52 @@ const Chat = ({ username, roomId, socket }) => {
   useEffect(scrollToBottom, [message]);
 
   return (
-    <>
+    <div className="flex flex-1 justify-center py-20 scrollbar-hide overflow-y-scroll">
       <Header title={roomId} socket={socket} />
-      <div className="flex flex-1 justify-center py-20">
-        <div className="flex flex-col overflow-scroll space-y-6">
-          {/* show messages from user */}
-          {message?.map((item, index) => {
-            if (item?.username === username) {
-              return (
-                <Chatbox
-                  key={index}
-                  message={item?.content}
-                  username={item?.username}
-                  currentUser
-                />
-              );
-            } else {
-              return (
-                <Chatbox
-                  key={index}
-                  message={item?.content}
-                  username={item?.username}
-                />
-              );
-            }
-          })}
+      <div className="flex flex-col space-y-6">
+        {/* show messages from user */}
+        {message?.map((item, index) => {
+          if (item?.username === username) {
+            return (
+              <Chatbox
+                key={index}
+                message={item?.content}
+                username={item?.username}
+                currentUser
+              />
+            );
+          } else {
+            return (
+              <Chatbox
+                key={index}
+                message={item?.content}
+                username={item?.username}
+              />
+            );
+          }
+        })}
 
-          {/* Scroll to bottom */}
-          <div ref={lastMessage} />
-        </div>
-        <div className="fixed bottom-8 w-screen">
-          <div className="flex justify-between bg-tint rounded-full p-2 mx-3">
-            <input
-              type="text"
-              placeholder="Message here..."
-              onChange={(e) => setInput(e.target.value)}
-              value={input}
-              className="outline-none bg-tint px-2 w-screen"
-            />
-            <button
-              onClick={sendMessage}
-              className="rounded-full bg-box text-white p-2"
-            >
-              <ArrowUpIcon width={20} height={20} />
-            </button>
-          </div>
+        {/* Scroll to bottom */}
+        <div ref={lastMessage} />
+      </div>
+      <div className="fixed bottom-8 w-screen">
+        <div className="flex justify-between bg-tint rounded-full p-2 mx-3">
+          <input
+            type="text"
+            placeholder="Message here..."
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            className="outline-none bg-tint px-2 w-screen"
+          />
+          <button
+            onClick={sendMessage}
+            className="rounded-full bg-box text-white p-2"
+          >
+            <ArrowUpIcon width={20} height={20} />
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
